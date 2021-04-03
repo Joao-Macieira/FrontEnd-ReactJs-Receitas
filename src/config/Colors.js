e0ad4d3473602f2ad53c44c2ffd146eaa0b960f6
João Macieira <jpfis2015@gmail.com>
2021-04-03 16:26:13 -0300

    Seleção das cores

--- src/config/Colors.js
+++ src/config/Colors.js
@@ -6,5 +6,6 @@ export const secondaryDarkColor = '#D97904';
 
 export const successColor = '#3E5902';
 export const infoColor = '#3E5902';
+
 export const errorColor = '#f2af29';
 export const warningColor = '#f2af29';
