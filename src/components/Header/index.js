9e23c4bcfa2b64d2a92b7a91c4a1e06c3cfb05d6
João Macieira <jpfis2015@gmail.com>
2021-04-03 15:55:55 -0300

    Rotas corrigidas

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
 import { Nav } from './styled';
 
 export default function Header() {
-  const botaoCliclado = useSelector((state) => state.example.botaoCliclado);
-
   return (
     <Nav>
       <Link to="/">
@@ -19,7 +16,6 @@ export default function Header() {
       <Link to="/eu">
         <FaSignInAlt size={24} />
       </Link>
-      {botaoCliclado ? 'Cliclado' : 'Não Cliclado'}
     </Nav>
   );
 }
