f9d36d6fa143359b493d79d6c5ac24795ebddcad
João Macieira <jpfis2015@gmail.com>
2021-04-04 10:48:59 -0300

    Criando rota de cadastro

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import MyRoute from './MyRoute';
 
 import Home from '../pages/Home';
 import Login from '../pages/Login';
+import Cadastro from '../pages/Cadastro';
 import Page404 from '../pages/Page404';
 
 export default function Routes() {
@@ -12,6 +13,7 @@ export default function Routes() {
     <Switch>
       <MyRoute exact path="/" component={Home} />
       <MyRoute exact path="/login" component={Login} />
+      <MyRoute exact path="/cadastro" component={Cadastro} />
       <MyRoute exact path="*" component={Page404} />
     </Switch>
   );
