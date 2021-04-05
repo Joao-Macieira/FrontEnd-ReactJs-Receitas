517296f619ef5551bb62dc14d2650270d41d3777
João Macieira <jpfis2015@gmail.com>
2021-04-05 11:38:57 -0300

    Página de edição de receitas

---
+++ src/pages/EditRecipe/index.js
@@ -0,0 +1,171 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
+
+import axios from '../../services/axios';
+import history from '../../services/history';
+
+import { Title, Container, Form } from './styled';
+
+export default function EditRecipe({ match }) {
+  const { id } = match.params;
+
+  const [categoryId, setCategoryId] = useState('');
+  const [name, setName] = useState('');
+  const [preparationTime, setPreparationTime] = useState('');
+  const [portions, setPortions] = useState('');
+  const [method, setMethod] = useState('');
+  const [ingredients, setIngredients] = useState('');
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get('/category');
+
+      setCategories(data);
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get(`/recipe/${id}`);
+
+      setCategoryId(data.id_categorias);
+      setName(data.nome);
+      setPreparationTime(data.tempo_preparo_minutos);
+      setPortions(data.porcoes);
+      setMethod(data.modo_preparo);
+      setIngredients(data.ingredientes);
+    })();
+
+    // eslint-disable-next-line
+  }, []);
+
+  function handleBack() {
+    history.push('/minha-conta');
+  }
+
+  async function handlerSubmit(e) {
+    e.preventDefault();
+
+    let formErrors = false;
+
+    if (!categoryId) {
+      toast.error('Selecione a categoria da receita');
+
+      formErrors = true;
+    }
+
+    if (!method) {
+      toast.error('Descreva o método de preparo da receita');
+
+      formErrors = true;
+    }
+
+    if (formErrors) return;
+
+    const { data } = await axios.put(`/recipe/${id}`, {
+      categoryId,
+      name,
+      preparationTime,
+      portions,
+      method,
+      ingredients,
+    });
+
+    if (data.error) {
+      toast.error(data.error);
+    } else {
+      history.push('/minha-conta');
+    }
+  }
+
+  return (
+    <Container>
+      <Form onSubmit={handlerSubmit}>
+        <Title>Melhore a sua Receita</Title>
+        <button className="backButton" type="button" onClick={handleBack}>
+          Página anterior
+        </button>
+        Categoria:
+        <select
+          value={categoryId}
+          onChange={(e) => setCategoryId(e.target.value)}
+        >
+          <option key={0} value="">
+            Selecione a categoria
+          </option>
+          {categories.map((cat) => (
+            <option key={cat.id} value={cat.id}>
+              {cat.nome}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="name">
+          Nome da Receita:{' '}
+          <input
+            type="text"
+            placeholder="Nome..."
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <div className="numberArea">
+          <label htmlFor="time">
+            Tempo de preparo (em minutos):{' '}
+            <input
+              type="number"
+              value={preparationTime}
+              placeholder="Tempo de preparo..."
+              onChange={(e) => setPreparationTime(e.target.value)}
+            />
+          </label>
+
+          <label htmlFor="portion">
+            Porções:{' '}
+            <input
+              type="number"
+              value={portions}
+              placeholder="Porções..."
+              onChange={(e) => setPortions(e.target.value)}
+            />
+          </label>
+        </div>
+        <label htmlFor="method">
+          Modo de preparo:{' '}
+          <small>
+            Separe o modo de preparo com vírgulas apenas, não use números, hífen
+            e etc
+          </small>
+          <textarea
+            value={method}
+            placeholder="Modo de preparo..."
+            onChange={(e) => setMethod(e.target.value)}
+          />
+        </label>
+        <label htmlFor="ingredients">
+          Ingredientes:{' '}
+          <small>
+            Separe os ingredientes com vírgulas APENAS, não use números, hífen e
+            etc
+          </small>
+          <textarea
+            value={ingredients}
+            placeholder="Ingredientes..."
+            onChange={(e) => setIngredients(e.target.value)}
+          />
+        </label>
+        <button type="submit">Melhorar</button>
+      </Form>
+    </Container>
+  );
+}
+
+EditRecipe.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
---
+++ src/pages/EditRecipe/styled.js
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+
+import * as colors from '../../config/Colors';
+
+export const Title = styled.h1`
+  color: ${colors.primaryColor};
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 12px auto;
+`;
+
+export const Form = styled.form`
+  width: 100%;
+  max-width: 600px;
+  background: #fff;
+  display: flex;
+  flex-direction: column;
+  border-radius: 8px;
+  font-size: 20px;
+  padding: 16px;
+
+  h1 {
+    text-align: center;
+  }
+
+  label {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+  }
+
+  select {
+    font-size: 16px;
+    padding: 4px 8px;
+    margin: 8px 4px;
+  }
+
+  input {
+    height: 40px;
+    font-size: 18px;
+    border: 1px solid #ddd;
+    padding: 0 10px;
+    border-radius: 4px;
+    margin-top: 5px;
+
+    &:focus {
+      border: 1px solid ${colors.primaryColor};
+    }
+  }
+
+  small {
+    margin: 8px 0;
+    font-size: 16px;
+    color: ${colors.infoColor};
+  }
+
+  textarea {
+    height: 160px;
+    resize: none;
+    font-size: 16px;
+    padding: 16px;
+  }
+
+  .backButton {
+    width: 100%;
+    max-width: 200px;
+    align-self: flex-end;
+    background: ${colors.primaryDarkColor};
+  }
+
+  .numberArea {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+`;
